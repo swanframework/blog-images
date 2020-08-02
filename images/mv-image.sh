367d6bd0c50aeb75166e84850ccd72cd09056a6f
zongf0504 <3154007684@qq.com>
2020-08-02 09:38:54 +0800

    新增移动图片脚本

--- images/mv-image.sh
+++ images/mv-image.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-#Desc 移动截图
+#Desc 移动截图文件夹中最新的.png截图至指定目录中，命名规则为: 目录名-yyyyMMddHHmm.png
 #Auth zongf
 #Date 2020-08-02
 
+# 目标目录
+target_dir=mybatis
 
 # 定义截图目录
-picDir=${HOME}/Pictures
-targetDir=./mybatis
+pic_dir=${HOME}/Pictures
 
 # 获取目录下最新的图片名
-latestImg=`ls -lt $picDir | grep .png | head  -n 1 | awk '{print $9}'`
+latest_img_name=`ls -lt $pic_dir | grep .png | head  -n 1 | awk '{print $9}'`
 
 #以当前时间命名
-newName=`date "+%Y%m%d%H%M%S"`.png
+new_img_name=$target_dir-`date "+%Y%m%d%H%M"`.png
 
 # 移动截图至新的文件夹
-mv $picDir/$latestImg $targetDir/$newName
+mv $pic_dir/$latest_img_name $target_dir/$new_img_name
