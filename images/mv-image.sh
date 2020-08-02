f3f74208931c32a03426d36daf0f35ed68e43f72
zongf0504 <3154007684@qq.com>
2020-08-02 10:04:33 +0800

    修改脚本

--- images/mv-image.sh
+++ images/mv-image.sh
@@ -16,4 +16,10 @@ latest_img_name=`ls -lt $pic_dir | grep .png | head  -n 1 | awk '{print $9}'`
 new_img_name=$target_dir-`date "+%Y%m%d%H%M"`.png
 
 # 移动截图至新的文件夹
-mv $pic_dir/$latest_img_name $target_dir/$new_img_name
+cp $pic_dir/$latest_img_name $target_dir/$new_img_name
+
+# 拼接git链接地址
+img_git_link="![image](https://raw.githubusercontent.com/zongf0504/blog-images/master/images/$target_dir/$new_img_name)"
+
+# 输出git连接地址
+echo $img_git_link
